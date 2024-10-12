509d76733cf80e1eeb30c84a0554dade64065719
thunderkite <hanasisgenya200710@gmail.com>
2024-10-12 22:41:45 +0300

    Добавьте файлы проекта.

---
+++ first_project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_project
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            Console.WriteLine("Введите сумму вклада:");
+            double sum = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите количество месяцев:");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= month)
+            {
+                i++;
+                double result = sum * 7 / 100;
+                if (i == month) Console.WriteLine(sum + result);
+            }
+        }
+    }
+}
